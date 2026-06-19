d26a97f645fe1d019ccc1820201bcf545905a045
Dag Himle <dag.himle@ad.eyeshare.no>
2026-06-19 13:44:14 +0200

    Adds notifications, file auto-reload, and 4-wide tabs
    - Adds mini.notify and routes vim.notify through it for toast notifications
    - Adds autoread with a checktime autocmd and a libuv timer to reload files changed on disk
    - Adds toasts for disk reloads and for LSP servers attaching
    - Sets tabstop and shiftwidth to 4

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -28,6 +28,8 @@ vim.opt.relativenumber = true
 vim.opt.scrolloff = 10
 vim.opt.termguicolors = true
 vim.opt.signcolumn = 'yes'
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
 -- Make split windows easy to tell apart; re-applied on every theme load
 -- mix two #rrggbb colors; t=0 -> a, t=1 -> b
@@ -52,6 +54,8 @@ vim.api.nvim_create_autocmd('ColorScheme', {
 vim.cmd.colorscheme('monokai-pro-octagon')
 require('mini.icons').setup()
 require('mini.icons').mock_nvim_web_devicons()
+require('mini.notify').setup()
+vim.notify = require('mini.notify').make_notify()
 require('gitsigns').setup({
 	on_attach = function(bufnr)
 		local gs = require('gitsigns')
@@ -70,6 +74,11 @@ vim.opt.smartcase = true
 
 -- Controls 
 vim.opt.mouse = 'a'
+
+-- Behaviour
+vim.opt.autoread = true
+vim.opt.updatetime = 200
+
 
 -- Diagnostics and automcomplete
 require('lazydev').setup()
@@ -133,13 +142,36 @@ require('which-key').setup()
 -- ---- Autocommands ---- 
 -- - yanking my chains
 local yank_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-
 vim.api.nvim_create_autocmd('TextYankPost', {
 	group = yank_group,
 	callback = function()
 		vim.hl.on_yank()
 	end,
 })
+
+-- Refresh
+local autoread_group = vim.api.nvim_create_augroup('AutoRead', { clear = true })
+vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
+	group = autoread_group,
+	callback = function()
+		vim.cmd('checktime')
+	end,
+})
+
+vim.api.nvim_create_autocmd('FileChangedShellPost', {
+	group = autoread_group,
+	callback = function()
+		vim.notify('Reloaded from disk', vim.log.levels.INFO)
+	end,
+})
+
+local timer = vim.uv.new_timer()
+if timer then
+	timer:start(1000, 1000, vim.schedule_wrap(function()
+		vim.cmd('checktime')
+	end))
+end
+
 
 
 -- ---- LSP ----
@@ -158,5 +190,17 @@ vim.lsp.config('lua_ls', {
 	},
 })
 vim.lsp.enable('lua_ls')
+
+-- announce language servers as they attach
+local lsp_group = vim.api.nvim_create_augroup('UserLsp', { clear = true })
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = lsp_group,
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client then
+			vim.notify('LSP attached: ' .. client.name, vim.log.levels.INFO)
+		end
+	end,
+})
 
 
